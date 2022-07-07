c5fbea8820b30a1b862d411552ea664173f984da
Toma Puljak <toma.puljak@hotmail.com>
2022-07-07 18:09:45 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM codeany/full
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a = 42;
+    cout << "Hello World!" << endl;
+    cout << a << endl;
+    
+    return 0;
+}
